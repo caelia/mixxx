9f1c727bf6f5c1b5865572084d61149d9929f41a
Caelia Chapin <crc@caelia.net>
2017-02-26 19:19:04 -0700

    Denon MCX8000: added JS code & bindings for shift button & performance pads.

--- res/controllers/Denon-MCX8000-scripts.js
+++ res/controllers/Denon-MCX8000-scripts.js
@@ -22,8 +22,22 @@ const DECK4 = 3;
 const LEFT = 0;
 const RIGHT = 1;
 
+// Pad modes
+const PM_CUE = 0;
+const PM_CUE_LOOP = 1;
+const PM_SHIFT_CUE = 2;
+const PM_ROLL = 3;
+const PM_SAVED_LOOP = 4;
+const PM_SLICER = 5;
+const PM_SLICER_LOOP = 6;
+const PM_SAMPLER = 7;
+const PM_VELOCITY_SAMPLER = 8;
+const PM_SHIFT_SAMPLER = 9;
+
+
 var Deck = function(id) {
     this.id = id;
+    this.padMode = null;
 }
 
 var ControlSet = function(controller, side) {
@@ -54,9 +68,61 @@ DenonMCX8000.init = function(id, debugging) {
         new Deck(DECK3),
         new Deck(DECK4)
     ];
+    var i;
+    for (i = 0; i < 4; i++) {
+        decks[i].padMode = PM_CUE;
+    }
     this.leftSide = new ControlSet(this, LEFT);
     this.rightSide = new ControlSet(this, RIGHT);
+    this.leftSide.activateDeck(DECK1);
+    this.rightSide.activateDeck(DECK2);
     this.decks = decks;
 }
 
 DenonMCX8000.shutdown = function() {}
+
+
+///////////////////////////////////////////////////////////////
+//                     MISCELLANEOUS                         //
+///////////////////////////////////////////////////////////////
+
+DenonMCX8000.shiftButton = function(channel, control, value, status, group) {
+    DenonMCX8000.shiftPressed = (value === 0x7f);
+}
+
+// DANGER!! Cutted & pasted from DDJ-SB2 script.
+
+///////////////////////////////////////////////////////////////
+//            HIGH RESOLUTION MIDI INPUT HANDLERS            //
+///////////////////////////////////////////////////////////////
+
+DenonMCX8000.highResMSB = {
+    '[Channel1]': {},
+    '[Channel2]': {},
+    '[Channel3]': {},
+    '[Channel4]': {}
+};
+
+DenonMCX8000.tempoSliderMSB = function(channel, control, value, status, group) {
+    DenonMCX8000.highResMSB[group].tempoSlider = value;
+};
+
+DenonMCX8000.tempoSliderLSB = function(channel, control, value, status, group) {
+    var fullValue = (DenonMCX8000.highResMSB[group].tempoSlider << 7) + value;
+    engine.setValue(
+        DenonMCX8000.deckSwitchTable[group],
+        'rate',
+        ((0x4000 - fullValue) - 0x2000) / 0x2000
+    );
+};
+
+
+///////////////////////////////////////////////////////////////
+//                 PERFORMANCE PAD HANDLERS                  //
+///////////////////////////////////////////////////////////////
+
+DenonMCX8000.perfPad = function(channel, control, value, status, group) {
+}
+
+DenonMCX8000.perfPadShift = function(channel, control, value, status, group) {
+}
